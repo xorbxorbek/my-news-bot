8986c8aa92ae375a839eb0d92dc03d04faf20faf
xorbxorbek <fkrmxorb@gmail.com>
2026-02-26 16:45:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ TELEGRAM_TOKEN = "8691397801:AAFJWMPazlZ1yI_T-N5UjetunAuHyrBJp6Q"
 CHAT_ID = "7519865650"
 KEYWORDS = ["현대로템 ESG", "탄소중립", "환경 규제", "LCA", "탄소발자국", "탄소세", "재생에너지", "넷제"]
 
-def get_google_news(keyword):
-    rss_url = f"https://news.google.com/rss/search?q={keyword}+when:1d&hl=ko&gl=KR&ceid=KR:ko"
+def get_google_news(keyword):# 띄어쓰기를 인터넷 주소 형식(%20)으로 변환해줍니다.
+    safe_keyword = keyword.replace(" ", "%20")
+    rss_url = f"https://news.google.com/rss/search?q={safe_keyword}+when:1d&hl=ko&gl=KR&ceid=KR:ko"
     feed = feedparser.parse(rss_url)
     results = []
     
@@ -17,16 +18,3 @@ def get_google_news(keyword):
         results.append(f"📌 {entry.title}\n🔗 {entry.link}")
     
     return results
-
-def send_telegram_msg(text):
-    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    params = {"chat_id": CHAT_ID, "text": text}
-    requests.get(url, params=params)
-
-# 실행부
-for kw in KEYWORDS:
-    news_list = get_google_news(kw)
-    if news_list:
-        send_telegram_msg(f"--- [{kw}] 뉴스 브리핑 ---")
-        for news in news_list[:3]: # 키워드당 상위 3개만
-            send_telegram_msg(news)
